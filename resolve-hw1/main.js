0e06ae16da1b487e26fa345bc74b2522e2fee55e
JeffreyDahmer <vitaliivyd@gmail.com>
2022-11-06 00:35:06 +0200

    resolve-hw1

---
+++ resolve-hw1/main.js
@@ -0,0 +1,41 @@
+// HW1
+let hello = 'hello';
+console.log(hello);
+let num = 1;
+console.log(num);
+let bool1 = true;
+console.log(bool1);
+let bool2 = false;
+console.log(bool2);
+const PI = 3.14;
+console.log(PI);
+const str = 'owu';
+console.log(str);
+
+hello = 'hello world';
+console.log(hello);
+
+bool1 = false;
+console.log(bool1);
+// HW1
+
+// HW2
+let firstName ='Vitalii';
+let middleName = 'Olegovuch';
+let lastName = 'Vyd';
+// let result = firstName + ' ' +middleName + ' ' + lastName;
+// console.log(result);
+let result = `${firstName} ${middleName} ${lastName}`;
+console.log(result);
+// HW2
+
+// HW3
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = 'true';
+console.log(typeof c);
+// HW3
+
+
